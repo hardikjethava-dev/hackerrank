194d065ebdd0fff7476d0f3c0dfc93066094aa2e
hardikjethava-dev <hardik.jethava92@gmail.com>
2025-12-16 19:40:56 +0530

    HelloWorld

---
+++ HelloWorldWithPython.py
@@ -0,0 +1,30 @@
+"""
+Problem: Hello, World!
+
+Description:
+    Write a program that prints the string "Hello, World!" to standard output.
+
+Input Format:
+    No input is required.
+
+Output Format:
+    Print the string "Hello, World!" to standard output.
+
+Sample Output:
+    Hello, World!
+"""
+
+
+# Solution 1
+
+if __name__ == '__main__':
+    print("Hello, World!")
+
+
+# Solution 2
+
+def main() -> None:
+    print("Hello, World!")
+
+if __name__ == '__main__':
+    main()
